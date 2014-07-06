37bf4d5e635b488e4aaecda77eea05885cea7553
Paul Nechifor <paul@nechifor.net>
2014-07-06 15:44:47 +0300

    Add Node Upstart script.

--- scripts/install-sites.py
+++ scripts/install-sites.py
@@ -47,13 +47,40 @@ def install_nechifor_node():
         }
     }
     """
+    install_nechifor_node_upstart()
     install_nginx_site('nechifor.net', content)
+
+def install_nechifor_node_upstart():
+    content = """#!upstart
+
+description "nechifor-site node server"
+author "Paul Nechifor <paul@nechifor.net>"
+
+start on startup
+stop on shutdown
+
+# Run as an unprivileged user.
+#setuid vagrant
+#setgid vagrant
+
+# Automatically respawn if it dies, but abandon if more than 5 times in 60 secs.
+respawn
+respawn limit 5 60
+
+script
+    exec /usr/bin/node /vagrant/sites/nechifor-site/build/app/app.js >> /var/log/nechifor-site 2>&1
+end script
+"""
+    f = open('/etc/init/nechifor-site.conf', 'w')
+    f.write(content)
+    f.close()
 
 def main():
     os.system('/opt/glassfish3/glassfish/bin/asadmin start-domain >/dev/null')
     remove_nginx_default()
     install_nechifor_ee()
     install_nechifor_node()
+    os.system('service nechifor-site restart')
     os.system('service nginx restart')
 
 if __name__ == '__main__':
