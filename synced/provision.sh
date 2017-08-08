379da1ca77fdbf6938d99bd11693a9c58f4efaeb
Paul Nechifor <paul@nechifor.net>
2017-08-08 22:28:05 +0100

    Start the container from provision.

--- synced/provision.sh
+++ synced/provision.sh
@@ -5,10 +5,14 @@ packages=(
     nginx
 )
 
+gitlab_image="gitlab/gitlab-ce:9.4.3-ce.0"
+
 main() {
     install_packages
 
     setup_docker
+    download_packages
+    start_containers
 }
 
 install_packages() {
@@ -17,7 +21,29 @@ install_packages() {
 }
 
 setup_docker() {
+    # Add the user to the docker group.
     gpasswd -a "$(getent passwd 1000 | cut -d: -f1)" docker
+}
+
+download_packages() {
+    docker pull "$gitlab_image"
+}
+
+start_containers() {
+    local vars=(
+        --detach
+        --hostname gitlab.nechifor.net
+        --publish 8081:80
+        # --publish 2222:22
+        --name gitlab
+        --restart always
+        --volume /opt/gitlab/config:/etc/gitlab
+        --volume /opt/gitlab/logs:/var/log/gitlab
+        --volume /opt/gitlab/data:/var/opt/gitlab
+        "$gitlab_image"
+    )
+
+    docker run "${vars[@]}"
 }
 
 main
